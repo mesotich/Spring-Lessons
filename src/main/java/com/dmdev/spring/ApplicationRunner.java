fa295b9bb1a2b5f5f5209a4e4cc2cedb87792f5d
Dmitry Shishkin <4144442@bk.ru>
2022-05-28 04:50:21 +0300

    LifeCicle CallBack - init & destroy

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -6,10 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var connectionPool = context.getBean("p1", ConnectionPool.class);
-        System.out.println(connectionPool);
-        var companyRepository = context.getBean("companyRepository");
-        System.out.println(companyRepository);
+        try (var context = new ClassPathXmlApplicationContext("application.xml");
+        ) {
+            //clazz->String->Map<String,Object>
+            var connectionPool = context.getBean("p1", ConnectionPool.class);
+            System.out.println(connectionPool);
+            var companyRepository = context.getBean("companyRepository");
+            System.out.println(companyRepository);
+        }
     }
 }
