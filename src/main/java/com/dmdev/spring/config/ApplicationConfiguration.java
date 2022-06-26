8d972e0954ea06a5dc8043f28cf8ece1e5674777
Dmitry Shishkin <4144442@bk.ru>
2022-06-26 20:25:50 +0300

    @Import & @ImportResource

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,24 +1,22 @@
 package com.dmdev.spring.config;
 
 import com.dmdev.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.dmdev.spring.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.dmdev.spring",
         useDefaultFilters = false,
-includeFilters = {
-        @Filter(type = FilterType.ANNOTATION,value = Component.class),
-        @Filter(type = FilterType.ASSIGNABLE_TYPE,value = CrudRepository.class),
-        @Filter(type = FilterType.REGEX,pattern = "com\\..+Repository")
-
-
-})
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
 public class ApplicationConfiguration {
 
 }
---
+++ src/main/java/com/dmdev/spring/web/config/WebConfiguration.java
@@ -0,0 +1,8 @@
+package com.dmdev.spring.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+
+}
