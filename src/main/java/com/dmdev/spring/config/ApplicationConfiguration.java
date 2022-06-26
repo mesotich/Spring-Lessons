ee8fa81cb98d9475b7311873ab66cd7eeabd66b1
Dmitry Shishkin <4144442@bk.ru>
2022-06-26 20:50:34 +0300

    @Bean Часть 1

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -19,4 +23,15 @@ import org.springframework.stereotype.Component;
         })
 public class ApplicationConfiguration {
 
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String userName,
+                                @Value("${db.pool.size}") Integer poolSize) {
+        return new ConnectionPool(userName, poolSize);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
